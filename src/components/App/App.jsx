8e00a11ee3c43d8de731cb06968bfc77f37b9ebf
Вікторія Юрченко <viktoriau26@gmail.com>
2021-09-09 14:55:32 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,9 @@
-// import './App.css';
 import React, { Component } from 'react';
 import { FeedbackOptions  } from '../Buttons/FeedbackOptions ';
 import { Statistics } from '../FeedbackSection/Statistics';
 import { MainContainer } from '../MainContainer/MainContainer'
-import { v4 as uuidv4 } from 'uuid';
+import { Section } from '../Section/Section';
+import { Notification } from '../Notification/Notification';
 import s from './App.module.css'
 
 export class App extends Component {
@@ -14,14 +14,11 @@ export class App extends Component {
   bad: 0
   }
   
-  totalFeedbacks = 0;
-  Percentage = 0;
-  
   countTotalFeedback = () => {
-    this.totalFeedbacks += 1;
+    return this.state.good + this.state.neutral + this.state.bad;
   };
   countPositiveFeedbackPercentage = () => {
-    this.Percentage = Math.round(this.state.good * 100 /this.totalFeedbacks);
+    return Math.round(this.state.good * 100 /this.countTotalFeedback());
   }
 
   handleClick = option => {
@@ -29,14 +26,12 @@ export class App extends Component {
       [option]: prevState[option] + 1
     }),
     );
-    this.countTotalFeedback();
-    this.countPositiveFeedbackPercentage();
   };
 
   render() {
-    const state = this.state;
-    const persentage = this.Percentage;
-    const total = this.totalFeedbacks;
+    const {good, neutral, bad} = this.state;
+    const persentage = this.countPositiveFeedbackPercentage();
+    const total = this.countTotalFeedback();
     return (
       <MainContainer>
         <h2 className={s.header}>Please leave feedback</h2>
@@ -46,24 +41,19 @@ export class App extends Component {
           onHandleClick={
             this.handleClick
           }
+        />
+        {total ? (
+          <Section title='Statistics'>
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positivePercentage={persentage}
           />
-        <h2 className={s.header}>Statistics</h2>
-        <ul className={s.StatsList}>
-          {Object.entries(state).map(([key, value]) => (
-            value > 0 &&
-            <li className={s.StatsItem}>
-              <span>{key}: {value}</span>
-            </li>
-          ))}
-          <span className={s.StatsResults}>In total: {total}</span>
-          <span className={s.StatsResults}>Positive feedback: {persentage} % </span>
-        </ul>
-        
-        {/* <Statistics
-          state={this.state}
-          persentage={this.Percentage}
-          total={this.totalFeedbacks}
-        /> */}
+        </Section>
+        )
+        : <Notification message="No feedback given"></Notification>}
       </MainContainer>
     );
   }
--- src/components/FeedbackSection/Statistics.jsx
+++ src/components/FeedbackSection/Statistics.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import s from './Statistics.module.css'
 
-export const Statistics = ({ state, total, persentage }) => {
-  <ul className="StatsName">
-    {Object.entries(state).map(([key, value]) => (
-      value > 0 &&
-      <li className="StatsItem">
-        <span>{key}: {value}</span>
-      </li>
-    ))}
-    <span>In total: {total}</span>
-    <span>Positive feedback: {persentage} % </span>
+export function Statistics({ good, neutral, bad, total, positivePercentage }) {
+  return (
+    <ul className={s.StatsList}>
+    <li className={s.StatsItem}>Good: {good}</li>
+    <li className={s.StatsItem}>Neutral: {neutral}</li>
+    <li className={s.StatsItem}>Bad: {bad}</li>
+    <li className={s.StatsResults}>In total: {total}</li>
+    <li className={s.StatsResults}>Positive feedback: {positivePercentage} % </li>
   </ul>
+  )
 }
